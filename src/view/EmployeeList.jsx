edb4afe86f5900ad49f3775a8cdc42573d15edb1
Miguel Rodrigues <miguel11rodrigues@gmail.com>
2025-05-29 14:03:25 +0100

    Alteração employeeList

--- src/view/EmployeeList.jsx
+++ src/view/EmployeeList.jsx
@@ -7,19 +7,20 @@ import Swal from "sweetalert2/dist/sweetalert2.js";
 import "sweetalert2/src/sweetalert2.scss";
 
 const urlAPI = "https://backend-ex-0yg7.onrender.com/employee/list";
+
 const EmployeeList = () => {
   const [dataEmployee, setdataEmployee] = useState([]);
+
   useEffect(() => {
     LoadEmployee();
   }, []);
+
   function LoadEmployee() {
-    const url = "https://backend-ex-0yg7.onrender.com/employee/list";
     axios
-      .get(url)
+      .get(urlAPI)
       .then((res) => {
         if (res.data.success) {
-          const data = res.data.data;
-          setdataEmployee(data);
+          setdataEmployee(res.data.data);
         } else {
           alert("Error Web Service!");
         }
@@ -28,103 +29,78 @@ const EmployeeList = () => {
         alert(error);
       });
   }
-  
-  
-  function OnDelete(id) {
-  Swal.fire({
-    title: "Are you sure?",
-    text: "You will not be able to recover this imaginary file!",
-    type: "warning",
-    showCancelButton: true,
-    confirmButtonText: "Yes, delete it!",
-    cancelButtonText: "No, keep it",
-  }).then((result) => {
-    if (result.value) {
-      SendDelete(id);
-    } else if (result.dismiss === Swal.DismissReason.cancel) {
-      Swal.fire("Cancelled", "Your imaginary file is safe :)", "error");
-    }
-  });
-}
-function SendDelete(userId) {
-  // url do backend
-  const baseUrl = "https://backend-ex-0yg7.onrender.com/employee/delete";
-  // network
-  axios
-    .post(baseUrl, {
-      id: userId,
-    })
-    .then((response) => {
-      if (response.data.success) {
-        Swal.fire("Deleted!", "Your employee has been deleted.", "success");
-        LoadEmployee();
-      }
-    })
-    .catch((error) => {
-      alert("Error 325 ");
-    });
-}
 
+  function OnDelete(id) {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You will not be able to recover this imaginary file!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, keep it",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        SendDelete(id);
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire("Cancelled", "Your imaginary file is safe :)", "error");
+      }
+    });
+  }
+
+  function SendDelete(userId) {
+    axios
+      .post("https://backend-ex-0yg7.onrender.com/employee/delete", { id: userId })
+      .then((response) => {
+        if (response.data.success) {
+          Swal.fire("Deleted!", "Your employee has been deleted.", "success");
+          LoadEmployee();
+        }
+      })
+      .catch(() => {
+        alert("Error 325 ");
+      });
+  }
 
   return (
     <table className="table table-hover table-striped">
       <thead className="thead-dark">
         <tr>
-          <th scope="col">#</th>
-          <th scope="col">Role</th>
-          <th scope="col">Name</th>
-          <th scope="col">Email</th>
-          <th scope="col">Address</th>
-          <th scope="col">Phone</th>
+          <th>#</th>
+          <th>Role</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Address</th>
+          <th>Phone</th>
           <th colSpan="2">Action</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th>1</th>
-          <td>Admin</td>
-          <td>Nuno Costa</td>
-          <td>ncosta@estgv.ipv.pt</td>
-          <td>Viseu</td>
-          <td>232480533</td>
-          <td>
-            <button className="btn btn-outline-info "> Edit </button>
-          </td>
-          <td>
-            <button className="btn btn-outline-danger "> Delete </button>
-          </td>
-        </tr>
-        <LoadFillData />
+        {dataEmployee.map((data, index) => (
+          <tr key={index}>
+            <th>{data.id}</th>
+            <td>{data.role.role}</td>
+            <td>{data.name}</td>
+            <td>{data.email}</td>
+            <td>{data.address}</td>
+            <td>{data.phone}</td>
+            <td>
+              <Link className="btn btn-outline-info" to={"employee/edit/" + data.id}>
+                Edit
+              </Link>
+            </td>
+            <td>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => OnDelete(data.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
-  function LoadFillData() {
-    return dataEmployee.map((data, index) => {
-      return (
-        <tr key={index}>
-          <th>{data.id}</th>
-          <td>{data.role.role}</td>
-          <td>{data.name}</td>
-          <td>{data.email}</td>
-          <td>{data.address}</td>
-          <td>{data.phone}</td>
-          <td>
-            <Link class="btn btn-outline-info " to={"employee/edit/" + data.id}>
-              Edit
-            </Link>
-          </td>
-          <td>
-            <button
-              className="btn btn-outlinedanger"
-              onClick={() => OnDelete(data.id)}
-            >
-              Delete
-            </button>
-          </td>
-        </tr>
-      );
-    });
-  }
 };
 
 export default EmployeeList;
